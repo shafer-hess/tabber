f8fecd5e7c7a5193113d40a354699fdc7aa74955
shafer_hess <shafer.hess01@gmail.com>
2022-08-03 21:49:39 -0400

    Created handler stubs, Added functionality to retrieve all browser tabs as well as the active tab. Added parsing logic to move command to proper handler

--- commands.js
+++ commands.js
@@ -1,3 +1,53 @@
-export default function handleCommand(command) {
-    console.log(command)
+// Returns all browser tabs and the currently active tab in the browser
+async function getBrowserTabs() {
+    // chrome.tabs api to query browser tabs
+    let tabs = await chrome.tabs.query({})
+    let [activeTab] = tabs.filter(tab => tab.active === true)
+
+    return [tabs, activeTab]
+}
+
+function moveTabLeft(tabs, activeTab) {
+
+}
+
+function moveTabRight(tabs, activeTab) {
+
+}
+
+function moveTabToFront(tabs, activeTab) {
+
+}
+
+function moveTabToBack(tabs, activeTab) {
+
+}
+
+function popOutTab(tabs, activeTab) {
+
+}
+
+// ENTRY: Function is passed a command by background.js, parse the command and call the appropriate handler function
+export default async function handleCommand(command) {
+    // Retrieve Browser tabs to be passed to handler function
+    let [tabs, activeTab] = await getBrowserTabs()
+        
+    // Parse received command
+    switch(command) {
+        case "move-tab-left":
+            moveTabLeft(tabs, activeTab)
+            break;
+        case "move-tab-right":
+            moveTabRight(tabs, activeTab)
+            break;
+        case "move-tab-to-front":
+            moveTabToFront(tabs, activeTab)
+            break;
+        case "move-tab-to-back":
+            moveTabToBack(tabs, activeTab)
+            break;
+        case "pop-out-tab":
+            popOutTab(tabs, activeTab)
+            break;
+    }
 }
